df5569c0cc62f0ca18657d0c1949fdb1df985a34
os.empty <goonbam852@gmail.com>
2026-09-07 17:24:42 +0900

    레거시 프로젝트 삭제

--- DirectXProj/Utils/IdGenerator.cpp
+++
@@ -1,3 +0,0 @@
-#include "IdGenerator.h"
-
-std::atomic<uint64_t> IdGenerator::s_counter = 0;
--- DirectXProj/Utils/json_exsample.hpp
+++
@@ -1,6 +0,0 @@
-// Please download the nlohmann/json library (a single header file)
-// from https://github.com/nlohmann/json and place its content here.
-//
-// For example, download json.hpp from:
-// https://github.com/nlohmann/json/releases/latest/download/json.hpp
-// and copy its contents into this file.
